7af9efea4d05e96ca767719e3d859ac6475ae34a
Connie Huang <huangc8@tcnj.edu>
2023-11-27 20:16:37 -0500

    Added content filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,21 @@
 module.exports = function (eleventyConfig) {
-
+	// Include assets in output folder.
 	eleventyConfig.addPassthroughCopy("src/assets/");
+
+	// Returns the first paragraph of a post.
+	eleventyConfig.addFilter("excerpt", (post) => {
+		const response = post;
+		const regex = /<p>(.*?)<\/p>/;
+		const corresp = regex.exec(response);
+		return corresp ? corresp[1] : "";
+	});
 
 	return {
 		dir: {
 			input: "src",
 			output: "public",
 			includes: "includes",
-			data: "data"
-		}
+			data: "data",
+		},
 	};
 };
